cd2c69511c2e46866114ca97ea9ebbe4d46c67a9
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-07-17 21:04:13 +0100

    Inventing

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,23 +1,38 @@
 #!/urs/bin/python3
-for i in range(100):
-    if i > 9:
-        i += 2
-        if i > 19:
-            i += 3
-            if i > 29:
-                i += 4
-                if i > 39:
-                    i += 5
-                    if i > 49:
-                        i += 6
-                        if i > 59:
-                            i += 7
-                            if i > 69:
-                                i += 8
-                                if i > 79:
-                                    i += 9
-                                    if i == 89:
-                                        print(i)
-                                        break
-
-    print(i, end=", ")
+for i in range(9):
+    for e in range(10):
+        if i == 1:
+            e += 2
+            if e > 9:
+                continue
+        elif i == 2:
+            e += 3
+            if e > 9:
+                continue
+        elif i == 3:
+            e += 4
+            if e > 9:
+                continue
+        elif i == 4:
+            e += 5
+            if e > 9:
+                continue
+        elif  i == 5:
+            e += 6
+            if e >  9:
+                continue
+        elif i == 6:
+            e += 7
+            if e > 9:
+                continue
+        elif i == 7:
+            e += 8
+            if e > 9:
+                continue
+        elif i == 8:
+            e += 9
+        if i == 8 and e == 9:
+            print("{}".format(i), end="")
+            print("{}".format(e))
+            break
+        print("{0}{1}".format(i,e), end=", ")
