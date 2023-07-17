d69357db4d5e3fb4a72ac2a8299f32001a3748db
Chijioke Michael Ezembadiwe <55343677+cj-flute@users.noreply.github.com>
2023-07-17 14:17:55 +0100

    00...99

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
-        print("0{0}, ".format(i), end="")
-    elif i == 99:
+    if i == 99:
         print(i)
     else:
-        print("{0}, ".format(i), end="")
+        print("{:02d}".format(i), end=", ")
